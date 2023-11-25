cd50e528651ca3c268d592663bf2c75b11a75374
ikromshi <numanovikrom454@gmail.com>
2023-11-25 01:17:25 -0500

    eval added

--- tsh.c
+++ tsh.c
@@ -166,6 +166,34 @@ int main(int argc, char **argv)
 */
 void eval(char *cmdline) 
 {
+    char *argv[MAXARGS]; // Argument list for execve()
+    char buf[MAXLINE];   // Holds modified command line
+    int bg;              // Should the job run in bg or fg?
+    pid_t pid;           // Process id
+
+    strcpy(buf, cmdline);
+    bg = parseline(buf, argv);
+    if (argv[0] == NULL)
+        return;   // Ignore empty lines
+
+    if (!builtin_cmd(argv)) {
+        if ((pid = fork()) == 0) {   // Child runs user job
+            if (execve(argv[0], argv, environ) < 0) {
+                printf("%s: Command not found.\n", argv[0]);
+                exit(0);
+            }
+        }
+
+        // Parent waits for foreground job to terminate
+        if (!bg) {
+            int status;
+            if (waitpid(pid, &status, 0) < 0)
+                unix_error("waitfg: waitpid error");
+        }
+        else {
+            printf("%d %s", pid, cmdline);
+        }
+    }
     return;
 }
 
